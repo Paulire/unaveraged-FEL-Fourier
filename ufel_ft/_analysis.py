bb318137215bec98a9b0da64ee12ce1623a8f5e7
Paul <paulire@btinternet.com>
2021-11-04 12:03:57 +0000

    Switched integration method for finding scaled energy to the simpson rule

--- ufel_ft/_analysis.py
+++ ufel_ft/_analysis.py
@@ -10,6 +10,6 @@ import numpy as np
 
 # Calculates the E feild as a function z by integrating A_out as a function of z_1 for each z point
 def _calc_E( self ):
-    from scipy.integrate import trapezoid
-    return np.array( [ trapezoid( np.abs( self.A_out[:,i] )**2, self.z_1 ) for i in range( self.current_save_index )] )
+    from scipy.integrate import simpson
+    return np.array( [ simpson( np.abs( self.A_out[:,i] )**2, self.z_1 ) for i in range( self.current_save_index + 1 )] )
 
