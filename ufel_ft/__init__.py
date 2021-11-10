a51d95f2e9a78f12aa29caacfc1c93e8b1433140
Paul <paulire@btinternet.com>
2021-11-10 14:03:38 +0000

    Adjusted the __init__() function to allow users to display the main verlibles by pressing tab in some code editors

--- ufel_ft/__init__.py
+++ ufel_ft/__init__.py
@@ -9,9 +9,30 @@ import _main
 import _plot
 import _fel_io
 
+import numpy as np
+
 class FEL_FT:
     # Set's up the user inputs into a usable manner
-    def __init__( self, dz=None, dz_1=None, N=None, l_e=None, n_bar=None, rho=None, chi=None, l_bar=None, delta_bar=0, fname=None, continue_from_file=False, z_end_file = None ):
+    def __init__( self, dz=None, dz_1=None, N=None, l_e=None, n_bar=None, rho=None, chi=None, l_bar=1, delta_bar=0, fname=None, continue_from_file=False, z_end_file = None ):
+        # Declarer each verlible so that the user may see the when pressing tab in a code editor
+        self.z = np.array( [0.0] )
+        self.continue_from_file = bool( None )
+        self.continue_from_file_z_value = int(0)
+        self.z_1 = np.array( [0.0] )
+        self.N_z = int(0)
+        self.N_z1 = int(0)
+        self.N = int(0)
+        self.l_e = float(0)
+        self.z_1_j = np.array( [0.0] )
+        self.n_bar = float(0)
+        self.rho = float(0)
+        self.l_bar = float(0)
+        self.delta_bar = float(0)
+        self.chi = np.zeros(1)
+        self.A = np.zeros( 1, dtype=complex ) #+ 1e-8            # Field amplitude
+        self.p_j = np.zeros( 1 )
+
+        # Settup the verlibles
         _setup.__init__( self, dz, dz_1, N, l_e, n_bar, rho, chi, l_bar, delta_bar, fname, continue_from_file, z_end_file )
     
     # Currently used to set the values of A and chi, should be moved to __init__
